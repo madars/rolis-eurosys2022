917db97b3392b1068d8a0eff2e1c2576243ed75f
Ubuntu <ubuntu@ip-10-0-80-205.ec2.internal>
2023-02-17 21:41:41 +0000

    fix

--- madars-set-ips.sh
+++ madars-set-ips.sh
@@ -3,9 +3,9 @@ IP_LEADER=$1
 IP_OHIO=$2
 IP_OREGON=$3
 
-echo $IP_LEADER > ip_leader_replica
-echo $IP_OHIO > ip_p1_follower_replica
-echo $IP_OREGON > ip_p2_follower_replica
+echo $IP_LEADER > scripts/ip_leader_replica
+echo $IP_OHIO > scripts/ip_p1_follower_replica
+echo $IP_OREGON > scripts/ip_p2_follower_replica
 
 for fn in third-party/paxos/config/1silo_1paxos_2follower/*.yml; do
     sed -i "s|localhost: .*\$|localhost: $IP_LEADER|" $fn
